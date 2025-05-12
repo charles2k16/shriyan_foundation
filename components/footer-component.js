0cb960a058f4adf73d71e163378513a5a0c35d1a
“charles2k16” <charlesprogram@gmail.com>
2025-05-12 14:59:26 +0000

    gallery

--- components/footer-component.js
+++ components/footer-component.js
@@ -8,17 +8,24 @@ Vue.component('site-footer', {
             <div
               class="col-xl-3 col-lg-3 col-md-6 wow fadeInUp2 animated"
               data-wow-delay=".1s">
+            
+                <div class="footer-log mt-25">
+                  <a href="index.html" class="footer-logo mb-30"
+                    ><img src="assets/img/logo/weblogo.png" alt="" width="300px"
+                  /></a>
+                </div>
+            
+            </div>
+            <div
+              class="col-xl-3 col-lg-3 col-md-6 wow fadeInUp2 animated"
+              data-wow-delay=".1s">
               <div class="footer__widget mb-30">
                 <h5 class="semi-02-title mb-25">About Shriyan</h5>
                 <p>
                   Shriyan Foundation empowers children and youth by providing essential
                   resources like school supplies, computers, sanitary pads, and more.
                 </p>
-                <div class="footer-log mt-25">
-                  <a href="index.html" class="footer-logo mb-30"
-                    ><img src="assets/img/logo/weblogo.png" alt="" width="300px"
-                  /></a>
-                </div>
+            
               </div>
             </div>
             <div
@@ -27,69 +34,21 @@ Vue.component('site-footer', {
               <div class="footer__widget fot_abot mb-30 pl-40">
                 <h5 class="semi-02-title mb-25">Quick Link</h5>
                 <ul class="fot-list">
-                  <li><a href="contact.html#">Our Organization</a></li>
-                  <li><a href="contact.html#">Become a Volunteer</a></li>
-                  <li><a href="contact.html#">Case Studies</a></li>
+                  <li><a href="about.html">Our Organization</a></li>
+                  <li><a href="volunteer.html">Become a Volunteer</a></li>
+            
 
-                  <li><a href="contact.html#">Recent Events</a></li>
+                  <li><a href="">Recent Events</a></li>
                 </ul>
               </div>
             </div>
-            <div
-              class="col-xl-3 col-lg-3 col-md-6 wow fadeInUp2 animated"
-              data-wow-delay=".5s">
-              <div class="footer__widget fot_abot mb-25">
-
-                <div class="insta-feed">
-                  <a class="insta" href="contact.html#">
-                    <img src="assets/img/gallery/IMG_1090.jpg" alt="" width="50px" />
-                  </a>
-                  <a class="insta" href="contact.html#">
-                    <img src="assets/img/gallery/IMG_1182 2.jpg" alt="" width="50px" />
-                    <span><i class="far fa-plus"></i></span>
-                  </a>
-                  <a class="insta" href="contact.html#">
-                    <img src="assets/img/gallery/IMG_1152 2.jpg" alt="" width="50px" />
-                    <span><i class="far fa-plus"></i></span>
-                  </a>
-                  <a class="insta" href="contact.html#">
-                    <img src="assets/img/gallery/IMG_1104.jpg" alt="" width="50px" />
-                  </a>
-                  <a class="insta" href="contact.html#">
-                    <img src="assets/img/gallery/IMG_1090.jpg" alt="" width="50px" />
-                    <span><i class="far fa-plus"></i></span>
-                  </a>
-                  <a class="insta" href="contact.html#">
-                    <img src="assets/img/gallery/IMG_1263.jpg" alt="" width="50px" />
-                    <span><i class="far fa-plus"></i></span>
-                  </a>
-                  <a class="insta" href="contact.html#">
-                    <img src="assets/img/gallery/IMG_1101.jpg" alt="" width="50px" />
-                    <span><i class="far fa-plus"></i></span>
-                  </a>
-                  <a class="insta" href="contact.html#">
-                    <img src="assets/img/gallery/IMG_1278.jpg" alt="" width="50px" />
-                    <span><i class="far fa-plus"></i></span>
-                  </a>
-                </div>
-              </div>
-            </div>
+     
             <div
               class="col-xl-3 col-lg-3 col-md-6 wow fadeInUp2 animated"
               data-wow-delay=".7s">
               <div class="footer__widget fot__subscribe mb-30">
-                <h5 class="semi-02-title mb-25">Our Newsletters</h5>
-                <p class="mb-25">
-                  But I must explain to you how all mistaken idea denouncing
-                </p>
-                <div class="form-area mb-30">
-                  <form
-                    action="https://www.devsnews.com/template/Shriyan/Shriyan/form.php">
-                    <input
-                      type="text"
-                      class="form-control"
-                      placeholder="Enter Your Email" />
-                  </form>
+                  <div class="copyright text-center">
+                  <p>Copyright © 2025, <span>Shriyan Foundation</span>.</p>
                 </div>
               </div>
             </div>
@@ -97,15 +56,7 @@ Vue.component('site-footer', {
           <!--scroll-target-btn-->
           <!-- <a href="#top-menu" class="scroll-target"><i class="far fa-arrow-up"></i></a> -->
           <!--scroll-target-btn-->
-          <div class="copy-right-area copy-area-02 pt-30">
-            <div class="row align-items-center">
-              <div class="col-xl-12">
-                <div class="copyright text-center">
-                  <p>Copyright © 2025, v2 <span>Shriyan</span>.</p>
-                </div>
-              </div>
-            </div>
-          </div>
+        
         </div>
       </footer>
   `,
