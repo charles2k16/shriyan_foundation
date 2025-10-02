0fd43f8bbcf13ae984fe1835f42d313d225b87ba
“charles-brij” <charles@brij.africa>
2025-10-02 09:31:03 +0000

    checkout page

--- components/news-component.js
+++ components/news-component.js
@@ -1,27 +1,27 @@
 Vue.component('site-news', {
   data() {
     return {
-      blogs: [
-        {
-          image: 'assets/img/gallery/IMG_1188.jpg',
-          title: 'Empowering the Future with Shriyan Foundation',
-          link: '#',
-          excerpt:
-            'Shriyan Foundation is a Ghana-based NGO committed to supporting underprivileged children and youth. Our mission is to empower them by providing school supplies, computers, sanitary pads, and other basic necessities that help them succeed academically and personally.',
-          date: '4 May 2025',
-          tag: 'Blog',
-        },
-        {
-          image: 'assets/img/blog/10.png',
-          title: 'The Heart Behind Shriyan Foundation',
-          link: '',
-          excerpt:
-            'What started as a simple desire to help has grown into something truly meaningful — Shriyan Foundation. Born out of compassion and a belief in equal opportunity, our foundation is dedicated to supporting children and youth who often go unnoticed.',
-          date: '4 May 2025',
-          tag: 'Projects',
-        },
-      ],
+      blogs: [], // will be filled with WordPress posts
     };
+  },
+  created() {
+    // Fetch WordPress posts
+    fetch(
+      'https://public-api.wordpress.com/rest/v1.1/sites/shriyanfoundation.wordpress.com/posts'
+    )
+      .then(res => res.json())
+      .then(data => {
+        // Map WP response into the format we need
+        this.blogs = data.posts.map(post => ({
+          image: post.featured_image || 'assets/img/blog/default.jpg', // fallback image
+          title: post.title,
+          link: post.URL,
+          excerpt: post.excerpt.replace(/<[^>]+>/g, ''), // remove HTML tags
+          date: new Date(post.date).toLocaleDateString(),
+          tag: post.tags ? Object.keys(post.tags)[0] : 'Blog',
+        }));
+      })
+      .catch(err => console.error('Error fetching posts:', err));
   },
   template: `
     <section class="blog-area pt-130 pb-75">
@@ -35,50 +35,20 @@ Vue.component('site-news', {
           </div>
         </div>
         <div class="row">
-          <div class="col-xl-6 col-lg-6 col-md-12" v-if="blogs[0]">
+          <div v-for="(blog, index) in blogs" :key="index" class="col-xl-6 col-lg-6 col-md-12">
             <div class="blog blog-03 mb-30">
               <div class="blog__thumb mb-20">
-                <img :src="blogs[0].image" alt="" />
+                <img :src="blog.image" alt="" />
               </div>
               <div class="blog__content">
                 <h3 class="blog-title mb-15">
-                  <a :href="blogs[0].link">{{ blogs[0].title }}</a>
+                  <a :href="blog.link" target="_blank">{{ blog.title }}</a>
                 </h3>
-                <p class="mb-15">{{ blogs[0].excerpt }}</p>
+                <p class="mb-15">{{ blog.excerpt }}</p>
                 <ul class="blog-author">
-                  <li><i class="far fa-calendar-alt"></i> {{ blogs[0].date }}</li>
+                  <li><i class="far fa-calendar-alt"></i> {{ blog.date }}</li>
+                  <li><i class="fas fa-tag"></i> {{ blog.tag }}</li>
                 </ul>
-              </div>
-            </div>
-          </div>
-          <div class="col-xl-6 col-lg-6 col-md-12">
-            <div class="row">
-              <div
-                class="col-xl-12 col-lg-6 col-md-6"
-                v-for="(blog, index) in blogs.slice(1)"
-                :key="index"
-              >
-                <div class="row blog blog-03 align-items-center mb-30">
-                  <div class="col-xl-6">
-                    <div class="blog__thumb pos-rel">
-                      <div class="blog__thumb--img">
-                        <img :src="blog.image" alt="" />
-                      </div>
-                      <a class="tag" v-if="blog.tag" :href="blog.link">{{ blog.tag }}</a>
-                    </div>
-                  </div>
-                  <div class="col-xl-6">
-                    <div class="blog__content">
-                      <h3 class="blog-title mb-15">
-                        <a :href="blog.link">{{ blog.title }}</a>
-                      </h3>
-                      <p class="mb-15">{{ blog.excerpt }}</p>
-                      <ul class="blog-author">
-                        <li><i class="far fa-calendar-alt"></i> {{ blog.date }}</li>
-                      </ul>
-                    </div>
-                  </div>
-                </div>
               </div>
             </div>
           </div>
